23bd0ed542eb963888713c0cb13a855f8eb17194
Mochamad Nurkhayal Kadafi <wowkadafi@gmail.com>
2020-05-06 08:06:46 +0700

    Initial Commit

---
+++ SpaceGameOOP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpaceGameOOP
+//
+//  Created by Mochamad Nurkhayal Kadafi on 06/05/20.
+//  Copyright © 2020 Manusia Apple. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
